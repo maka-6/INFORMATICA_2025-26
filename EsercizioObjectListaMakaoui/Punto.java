fb70858a93e2e2994bcbb0e8f0ac2cb141e3fe19
maka-6 <makaouiyouness30@gmail.com>
2025-12-19 18:29:27 +0100

    Aggiunta esercizio vacanze di natale Lista02

---
+++ EsercizioObjectListaMakaoui/Punto.java
@@ -0,0 +1,45 @@
+/*
+ * Autore: Makaoui Youness
+ * Data: 17/12/2025
+ * Classe: 4G
+ * Luogo: xx
+ * Descrizione: Lista dinamica in java
+ */
+
+class Point {
+
+    private float x;
+    private float y;
+    public String name;
+
+    Point(){
+        y = 0;
+        x = 0;
+        name = "Origine";
+    }
+
+    Point ( float x, float y, String name ){
+        this.name = name;
+        this.x = x;
+        this.y = y;
+    }
+
+    float getX() {
+        return this.x;
+    }
+    float getY() {
+        return this.y;
+    }
+    String getName(){
+        return name;
+    }
+
+    float Distance ( float x, float y ){
+        return (float) Math.sqrt(Math.pow( this.x - x, 2) + Math.pow( this.y - y, 2));
+    }
+
+    @Override
+    public String toString(){
+        return getName()+" ("+getX()+";"+getY()+")";
+    }
+}
