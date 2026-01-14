749ed1cc6bff16f4da275bd69831ec03f5a1068e
Youness makaoui <119128296+maka-6@users.noreply.github.com>
2026-01-14 19:34:38 +0100

    Compito ricerca binaria

--- RicercaBinaria/src/Main.java
+++ RicercaBinaria/src/Main.java
@@ -1,13 +1,78 @@
 /*
  * Autore: Makaoui Youness
- * Data: 29/9/2025
+ * Data: 14/01/2026
  * Luogo: xx
  * Descrizione:
  */
 
+import java.util.Arrays;
+import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
+        //
+        Random random = new Random();
+        // array da 10 elementi popolato randomicamente
+        int[] array = new int[10];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * random.nextInt(100);
+        }
 
+        //ordinamento dell'array selection sort
+        for(int i=0;i<array.length;i++){
+            for(int j=i+1;j<array.length;j++){
+                if(array[i]>array[j]){
+                    int temp = array[i];
+                    array[i]=array[j];
+                    array[j]=temp;
+                }
+            }
+        }
+        // numero da trovare
+        int num = random.nextInt(100);
+        // per il primo caso inserisco il numero da trovare all'interno dell'array.
+        array[random.nextInt(array.length)] = num;
+
+        System.out.println("Array: "+Arrays.toString(array));
+        System.out.println("Numero: "+num);
+
+        int min = 0;
+        int max = array.length-1;
+        int med = (min+max)/2;
+
+        // caso 1 con numero sempre presente all'interno dell'array
+        while(min<=max){
+            if(array[med]==num){
+                System.out.println("Trovato!");
+                break;
+            }else if(array[med]<num){
+                min=med;
+            }else{
+                max=med;
+            }
+            med=(min+max)/2;
+        }
+
+        // caso 2
+        for (int i = 0; i < array.length; i++) {
+            array[i] = i * random.nextInt(100);
+        }
+
+        System.out.println("Array: "+Arrays.toString(array));
+        System.out.println("Numero: "+num);
+
+        // caso 2 con numero non sempre presente all'interno dell'array
+        while(med<max||med>min){
+            if(array[med]==num){
+                System.out.println("Trovato!");
+                break;
+            }else if(array[med]<num){
+                min=med;
+            }else{
+                max=med;
+            }
+            med=(min+max)/2;
+        }
+        System.out.println("Numero non trovato!");
     }
 }
